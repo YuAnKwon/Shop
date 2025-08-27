4ae6abcac5cf0c5bf473e85a75900a818ddc178d
YuAnKwon <158270520+YuAnKwon@users.noreply.github.com>
2025-08-28 08:57:15 +0900

    Update ItemRepositoryCustom.java

--- src/main/java/com/shop/repository/item/ItemRepositoryCustom.java
+++ src/main/java/com/shop/repository/item/ItemRepositoryCustom.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ItemRepositoryCustom {
-
     // 페이징정보를 담고있는 pageable객체. 반환데이터로 Page<Item> 객체.
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
     Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
